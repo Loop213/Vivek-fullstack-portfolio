5539f67892a53f50d51b5eb63ee790fc4528a518
Vivek Singh <singhvivek2103@Mac-14.lan>
2026-04-25 13:35:46 +0530

    Added new features / updated code

--- client/vite.config.js
+++ client/vite.config.js
@@ -21,10 +21,11 @@ export default defineConfig({
     }
   },
   server: {
+    host: "127.0.0.1",
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://127.0.0.1:5001",
         changeOrigin: true
       }
     }
